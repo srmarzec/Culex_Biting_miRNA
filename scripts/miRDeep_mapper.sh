7494dfc3c1f75bb5f0f99f7d71c58659d3ee8bcb
srmarzec <srmarzec@users.noreply.github.com>
2022-07-08 12:47:59 -0400

    Update miRDeep_mapper.sh

--- scripts/miRDeep_mapper.sh
+++ scripts/miRDeep_mapper.sh
@@ -5,25 +5,27 @@
 #SBATCH --mem=4G
 
 #-----------------------------------------------------------------------------#
-# This script runs mapper.pl to make an ARF file for miRDeep2 #
+# This script runs miRDeep2 #
 #-----------------------------------------------------------------------------#
 
-#- Go into Conda environment where mapper.pl is  --------------------------------#
+#- Go into Conda environment where miRDeep2.pl is  --------------------------------#
 
 source activate conda-env
 
+cd /home/sm3679/culex_smallRNA/miRDeep_dir_run2/
+
 #- Set variables ----------------------------------------------------------------#
 
-config_file=/home/sm3679/culex_smallRNA/miRDeep_dir/config_file.txt
-genome_index=/home/sm3679/culex_smallRNA/genome/indexed/Cqu
-output_reads=/home/sm3679/culex_smallRNA/miRDeep_dir/processed_reads.fa
-output_arf=/home/sm3679/culex_smallRNA/miRDeep_dir/reads_vs_genome.arf
+genome=/home/sm3679/culex_smallRNA/genome/Culex_genome_noWhiteSpace.fa
+reads=/home/sm3679/culex_smallRNA/miRDeep_dir/processed_reads.fa
+arf=/home/sm3679/culex_smallRNA/miRDeep_dir/reads_vs_genome.arf
+known_miRNA=/home/sm3679/culex_smallRNA/known_miRNAs/unique_miRNAs.fa
+known_precursor=/home/sm3679/culex_smallRNA/known_miRNAs/precursor_miRNA_noSapce.fa
 
 
-#- RUN mapper.pl ----------------------------------------------------------------#
+#- RUN miRDeep2.pl ----------------------------------------------------------------#
 
-mapper.pl ${config_file} -d -e -h -j -l 18 -m -p ${genome_index} -s ${output_reads} -t ${output_arf}
-
+miRDeep2.pl ${reads} ${genome} ${arf} ${known_miRNA} none ${known_precursor} 2> miRDeep2_report.log
 
 
 #- FIN -----------------------------------------------------------------------#
